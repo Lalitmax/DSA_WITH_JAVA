b151a392d9afdf832604d7d9b67609bf38f08675
Lalit Max <lalitmay@gmail.com>
2024-02-03 17:00:10 +0530

    d

--- DSA Practice Problems/graph_practice.java
+++ DSA Practice Problems/graph_practice.java
@@ -5,9 +5,25 @@ import java.util.Queue;
 public class graph_practice {
 
     public static class Edge{
+        int src,dest;
 
+        public Edge(int src, int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int V = 5;
+
+
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        
         
     }
  
     
 }
+        
