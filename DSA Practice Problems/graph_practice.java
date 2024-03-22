bad690c28360a117c841f5862b7864c6b6f5f571
ankit1275 <Ankit1275.be22@chitkara.edu.in>
2024-03-22 11:58:21 +0530

    Practice

--- DSA Practice Problems/graph_practice.java
+++ DSA Practice Problems/graph_practice.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.*;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -16,44 +17,47 @@ public class graph_practice {
         }
     }
 
-    public static void dfs(ArrayList<Edge>[] graph, int src, boolean v[]) {
-        v[src] = true;
 
-        System.out.println(src + " ");
+    public static void bfs(ArrayList<Edge>[] graph){
 
-        for (int i = 0; i < graph[src].size(); i++) {
-            int curr = graph[src].get(i).dest;
-
-            if (!v[curr]) {
-                dfs(graph, curr, v);
-            }
-        }
-
-    }
-
-    public static void bft(ArrayList<Edge>[] graph) {
-
-        boolean[] v = new boolean[graph.length];
-
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer>  q = new LinkedList<>();
 
         q.add(0);
+        boolean visited[]=new boolean[graph.length];
+        Arrays.fill(visited , false);
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
+            int  curr = q.poll();
 
-            int curr = q.remove();
+            if(!visited[curr]){
+                visited[curr]=true;
+                System.out.println(curr+" ");
+                for(int i=0; i<graph[curr].size(); i++){
+                    int dest = graph[curr].get(i).dest;
 
-            if (!v[curr]) {
-                System.out.print(curr + " ");
-                v[curr] = true;
-                for (int i = 0; i < graph[curr].size(); i++) {
-                    if (!v[graph[curr].get(i).dest]) {
-                        q.add(graph[curr].get(i).dest);
+                    if(!visited[dest]){
+                        q.add(dest);
                     }
                 }
-
             }
+
         }
+       
+    }
+
+    public static  void dfs(ArrayList<Edge>[] graph, int src, boolean[] visited){
+        visited[src]=true;
+
+        System.out.println(src+" ");
+        for(int i=0; i<graph[src].size(); i++) {
+            int dest = graph[src].get(i).dest;
+
+            if(!visited[dest]){
+                dfs(graph, dest, visited);
+            }
+
+        }
+
     }
 
     public static void main(String[] args) {
@@ -62,6 +66,8 @@ public class graph_practice {
         int V = 7;
 
         ArrayList<Edge>[] graph = new ArrayList[V];
+        boolean visited[]=new boolean[graph.length];
+        Arrays.fill(visited,false);
 
         // yet not initialized
 
@@ -69,145 +75,30 @@ public class graph_practice {
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0,1));
+        graph[0].add(new Edge(0,2));
 
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,3));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,4));
 
-        graph[3].add(new Edge(3, 1));
-        // graph[3].add(new Edge(3,5));
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,5));
 
-        graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4,2));
+        graph[4].add(new Edge(4,5));
 
-        // graph[6].add(new Edge(6, 5));
+        graph[5].add(new Edge(5,3));
+        graph[5].add(new Edge(5,4));
+        graph[5].add(new Edge(5,6));
 
-        // graph[0].add(new Edge(0,1));
-        // graph[0].add(new Edge(0,2));
+        graph[6].add(new Edge(6,5));
+        // bfs(graph);
+        dfs(graph, 0, visited);
 
-        // graph[1].add(new Edge(1,0));
-        // graph[1].add(new Edge(1,3));
-
-        // graph[2].add(new Edge(2,0));
-        // graph[2].add(new Edge(2,4));
-
-        // graph[3].add(new Edge(3,1));
-        // graph[3].add(new Edge(3,4));
-        // graph[3].add(new Edge(3,5));
-
-        // graph[4].add(new Edge(4, 2));
-        // graph[4].add(new Edge(4, 3));
-        // graph[4].add(new Edge(4, 5));
-
-        // graph[5].add(new Edge(5,3));
-        // graph[5].add(new Edge(5,4));
-        // graph[5].add(new Edge(5,6));
-
-        // graph[6].add(new Edge(6, 5));
-
-        // printSrcToTar(graph, 0, new boolean[V], "0", 5);
-        System.out.println(detectCycle(graph));
-
-    }
-
-    public static boolean isBp(ArrayList<Edge>[] graph){
-
-        int colors[] = new int[graph.length];
-
-        for(int i=0; i<graph.length; i++){
-            colors[i]=-1;
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i=0; i<graph.length; i++){
-            if(colors[i]==-1){
-                q.add(i);
-                colors[i]=0; // yellow
-
-                while(!q.isEmpty()){
-
-                    int curr = q.remove();
-
-                    for(int j=0; j<graph[curr].size(); j++){
-                        int dest  = graph[curr].get(j).dest;
-
-                        if(colors[dest]==-1){
-                            int nextColor = colors[curr] == 0 ? 1 : 0;
-                            colors[dest]=nextColor;
-                            q.add(dest);
-                        }else if(colors[dest] ==colors[curr]){
-                            return false;
-
-                        }
-
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
-
-    public static boolean detectCycle(ArrayList<Edge>[] graph) {
-
-        boolean[] v = new boolean[graph.length];
-
-        for (int i = 0; i < graph.length; i++) {
-            if (!v[i]) {
-                if (detectCycleUtil(graph, i, -1, v)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, int curr, int par, boolean[] v) {
-
-        v[curr] = true;
-
-        for (int i = 0; i < graph[curr].size(); i++) {
-
-            int dest = graph[curr].get(i).dest;
-
-            if (!v[dest]) {
-                if (detectCycleUtil(graph, dest, curr, v)) {
-                    return true;
-                }
-            } else if (dest != par) {
-                return true;
-            }
-
-        }
-
-        return false;
-
-    }
-
-    public static void printSrcToTar(ArrayList<Edge> graph[], int src, boolean v[], String str, int tar) {
-
-        v[src] = true;
-        if (src == tar) {
-            System.out.println(str);
-            return;
-        }
-
-        for (int i = 0; i < graph[src].size(); i++) {
-
-            int curr = graph[src].get(i).dest;
-
-            if (!v[curr]) {
-
-                printSrcToTar(graph, curr, v, str + curr, tar);
-                v[curr] = false;
-            }
-        }
+ 
 
     }
 
